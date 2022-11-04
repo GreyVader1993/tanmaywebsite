b7cf21479b44ddc388f957ec5b505c5a8dd5db9a
Tanmay Dureja <tanmaydureja@gmail.com>
2022-11-04 08:55:59 -0700

    New Footer and Banner for Home

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -4,38 +4,11 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
-        {/* Top area: Blocks */}
-        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
-
-          {/* 2nd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Products</h6>
-          </div>
-
-          {/* 3rd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
-          </div>
-
-          {/* 4th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Company</h6>
-          </div>
-
-          {/* 5th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
-            <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
-          </div>          
-
-        </div>
-
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 py-6 bg-black">
         {/* Bottom area */}
         <div className="md:flex md:items-center md:justify-center py-4 md:py-8 border-t border-gray-200">
-
           {/* Social links */}
-          <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
+          <ul className="flex mb-4 md:order-1 md:mb-0">
             <li>
               <Link to="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
